498cfa4afe77769c538b80a5ecfcb746325ef1b7
Joey Leingang <leingang@arizona.edu>
2012-08-23 15:52:47 -0700

    convert from svn

---
+++ project/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import *
+from django.contrib import admin
+from libscampi.contrib import cms
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^comments/', include('django.contrib.comments.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'', include(cms.site.urls)) 
+)
